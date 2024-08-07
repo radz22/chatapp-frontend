6d389d0a0bc490e0e0d1b30e7d741c898bea3fe7
radz22 <radzsantillan01@gmail.com>
2024-08-08 00:42:48 +0800

    change

--- src/context/SocketContext .jsx
+++ src/context/SocketContext .jsx
@@ -6,7 +6,7 @@ const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = io("http://localhost:5000");
+    const newSocket = io("https://chatapp-backend-ckwv.onrender.com");
 
     setSocket(newSocket);
 
